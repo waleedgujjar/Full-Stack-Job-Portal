fd4110c95199fd8c3fb20c6ca2a863874660fd75
waleed <gujjarwaleed221@gmail.com>
2025-03-13 11:21:20 +0500

    Dist Changes

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -1,13 +1,18 @@
-/* eslint-disable no-undef */
-import path from "path"
-import react from "@vitejs/plugin-react"
-import { defineConfig } from "vite"
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes("node_modules")) {
+            return "vendor"; // Moves dependencies to a separate chunk
+          }
+        },
+      },
     },
+    chunkSizeWarningLimit: 1000, // Increases warning limit (optional)
   },
-})
+});
